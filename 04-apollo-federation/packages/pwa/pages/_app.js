b803f5dfa5a7912965b03ce155b5869579912250
rober <me@rober.dev>
2020-07-29 10:31:08 +0200

    Added material-ui to pwa project

--- 04-apollo-federation/packages/pwa/pages/_app.js
+++ 04-apollo-federation/packages/pwa/pages/_app.js
@@ -1,16 +1,46 @@
 // Vendor libs
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
+
+// Material-UI theme
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../lib/theme';
+
+// Custom libs
 import { useApollo } from '../lib/apolloClient';
 
 // BaseApp definition
 const BaseApp = ({ Component, pageProps }) => {
   const apolloClient = useApollo(pageProps.initialApolloState);
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <Head>
+        <title>Page title base</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <ApolloProvider client={apolloClient}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </ThemeProvider>
+    </>
   );
 };
 
--- 04-apollo-federation/packages/pwa/pages/index.js
+++ 04-apollo-federation/packages/pwa/pages/index.js
@@ -2,14 +2,32 @@
 import React from 'react';
 import Link from 'next/link';
 
+// Material-UI components
+import Button from '@material-ui/core/Button';
+
 // Component definition
 const HomePage = () => {
   return (
     <div>
       <h1>Hello world</h1>
-      <Link href='/movies'>
-        <a href='/movies'>Películas</a>
-      </Link>
+
+      <div>
+        <Link href='/movies'>
+          <a href='/movies'>Películas</a>
+        </Link>
+      </div>
+
+      <div>
+        <Link href='/products/list'>
+          <a href='/products/list'>Productos</a>
+        </Link>
+      </div>
+
+      <div>
+        <Button variant='contained' color='primary'>
+          Primary
+        </Button>
+      </div>
     </div>
   );
 };
